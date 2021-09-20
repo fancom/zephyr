4007a020587aff2941780a9329b80c03d66db0e7
Jeroen van Dooren <jeroen.van.dooren@nobleo.nl>
2021-09-20 11:24:51 +0200

    Add CMC10 board config

---
+++ boards/arm/cmc10_h723zg/board.cmake
@@ -0,0 +1,6 @@
+board_runner_args(jlink "--device=STM32H723ZG" "--speed=4000")
+board_runner_args(stm32cubeprogrammer "--port=swd" "--reset=hw")
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
