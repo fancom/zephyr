6487a10dc67e327d3205994727bb3752e67d7b21
Jeroen van Dooren <jeroen.van.dooren@nobleo.nl>
2023-02-09 18:17:06 +0100

    Adding example to prove loss of event

---
+++ samples/hello_bug/src/main.c
@@ -0,0 +1,112 @@
+/* Networking DHCPv4 client */
+
+/*
+ * Copyright (c) 2017 ARM Ltd.
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(net_dhcpv4_mdns_bug, LOG_LEVEL_DBG);
+
+#include <zephyr/kernel.h>
+#include <zephyr/linker/sections.h>
+#include <errno.h>
+#include <stdio.h>
+
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/net_core.h>
+#include <zephyr/net/net_context.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/net/net_config.h>
+#include <zephyr/drivers/gpio.h>
+
+static struct net_mgmt_event_callback mgmt_cb;
+
+K_SEM_DEFINE(my_sem, 0, 1);
+
+static void handler(struct net_mgmt_event_callback *cb,
+		    uint32_t mgmt_event,
+		    struct net_if *iface)
+{
+	int i = 0;
+
+	if (mgmt_event != NET_EVENT_IPV4_ADDR_ADD) {
+		return;
+	}
+
+	for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+		char buf[NET_IPV4_ADDR_LEN];
+
+		if (iface->config.ip.ipv4->unicast[i].addr_type !=
+							NET_ADDR_DHCP) {
+			continue;
+		}
+
+		LOG_INF("Your address: %s",
+			net_addr_ntop(AF_INET,
+			    &iface->config.ip.ipv4->unicast[i].address.in_addr,
+						  buf, sizeof(buf)));
+		LOG_INF("Lease time: %u seconds",
+			 iface->config.dhcpv4.lease_time);
+		LOG_INF("Subnet: %s",
+			net_addr_ntop(AF_INET,
+				       &iface->config.ip.ipv4->netmask,
+				       buf, sizeof(buf)));
+		LOG_INF("Router: %s",
+			net_addr_ntop(AF_INET,
+						 &iface->config.ip.ipv4->gw,
+						 buf, sizeof(buf)));
+		k_sem_give(&my_sem);
+	}
+}
+
+void main(void)
+{
+	struct net_if *iface;
+	int ret;
+
+	LOG_INF("Run hello-bug");
+
+	gpio_pin_configure(
+		DEVICE_DT_GET_OR_NULL(DT_GPIO_CTLR(DT_NODELABEL(ethsw_reset), gpios)),
+		DT_GPIO_PIN(DT_NODELABEL(ethsw_reset), gpios),
+            	GPIO_OUTPUT_INACTIVE | DT_GPIO_FLAGS(DT_NODELABEL(ethsw_reset), gpios));
+
+	net_mgmt_init_event_callback(&mgmt_cb, handler,
+				     NET_EVENT_IPV4_ADDR_ADD | NET_EVENT_IPV4_MCAST_JOIN);
+	net_mgmt_add_event_callback(&mgmt_cb);
+
+	iface = net_if_get_default();
+
+	LOG_INF("Bringing the interface down");
+	ret = net_if_down(iface);
+        if (ret) {
+		LOG_ERR("Failed to bring network interface down: %d", ret);
+            	return;
+        }
+
+	LOG_INF("Bringing the interface up");
+        ret = net_if_up(iface);
+        if (ret) {
+            LOG_ERR("Failed to bring network interface up again: %d", ret);
+            return;
+        }
+
+	LOG_INF("Configuring the interface");
+	ret = net_config_init_by_iface(iface, "hello-bug-if", NET_CONFIG_NEED_IPV4,
+                                           CONFIG_NET_CONFIG_INIT_TIMEOUT * MSEC_PER_SEC);
+        if (ret) {
+            LOG_ERR("Failed to init network: %d", ret);
+            return;
+        }
+
+	LOG_INF("Waiting for dhcpv4 assigned IP address");
+	if (k_sem_take(&my_sem, K_MINUTES(1)) != 0) {
+		LOG_ERR("Failed to receive a dhcpv4 assigned IP address: %d", ret);
+	}
+	else {
+		LOG_INF("Success!");
+	}
+}
--- subsys/net/ip/net_mgmt.c
+++ subsys/net/ip/net_mgmt.c
@@ -1,3 +1,453 @@
+#if 0
+/*
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(net_mgmt, CONFIG_NET_MGMT_EVENT_LOG_LEVEL);
+
+#include <zephyr/kernel.h>
+#include <zephyr/toolchain.h>
+#include <zephyr/linker/sections.h>
+
+#include <zephyr/sys/util.h>
+#include <zephyr/sys/slist.h>
+#include <zephyr/net/net_mgmt.h>
+#include <zephyr/debug/stack.h>
+
+#include "net_private.h"
+
+struct mgmt_event_entry {
+	uint32_t event;
+	struct net_if *iface;
+
+#ifdef CONFIG_NET_MGMT_EVENT_INFO
+	uint8_t info[NET_EVENT_INFO_MAX_SIZE];
+	size_t info_length;
+#endif /* CONFIG_NET_MGMT_EVENT_INFO */
+};
+
+struct mgmt_event_wait {
+	struct k_sem sync_call;
+	struct net_if *iface;
+};
+
+static K_SEM_DEFINE(network_event, 0, K_SEM_MAX_LIMIT);
+static K_MUTEX_DEFINE(net_mgmt_lock);
+
+K_KERNEL_STACK_DEFINE(mgmt_stack, CONFIG_NET_MGMT_EVENT_STACK_SIZE);
+static struct k_thread mgmt_thread_data;
+static struct mgmt_event_entry events[CONFIG_NET_MGMT_EVENT_QUEUE_SIZE];
+static uint32_t global_event_mask;
+static sys_slist_t event_callbacks = SYS_SLIST_STATIC_INIT(&event_callbacks);
+static uint16_t head;
+static uint16_t tail;
+
+static bool cbInvocationOngoing = false;
+
+
+static inline bool is_event_queue_full(void) {
+
+	/* We need to handle the wraparound case */
+	uint16_t next = head + 1;
+    	if (next == CONFIG_NET_MGMT_EVENT_QUEUE_SIZE) {
+		next = 0;
+   	}
+
+	return next == tail;
+}
+
+static inline bool is_event_queue_empty(void) {
+	// We define empty as head == tail */
+    	return (head == tail);
+}
+
+static inline void advance_event_queue_pointer(void) {
+	if(is_event_queue_full()) {
+		if (++tail == CONFIG_NET_MGMT_EVENT_QUEUE_SIZE) {
+			tail = 0;
+		}
+	}
+
+	if (++head == CONFIG_NET_MGMT_EVENT_QUEUE_SIZE) {
+		head = 0;
+	}
+}
+
+static inline void retreat_event_queue_pointer(void)
+{
+	/* We need to handle the wraparound case */
+	if (++tail == CONFIG_NET_MGMT_EVENT_QUEUE_SIZE) {
+		tail = 0;
+	}
+}
+
+static inline void mgmt_push_event(uint32_t mgmt_event, struct net_if *iface,
+				   const void *info, size_t length)
+{
+#ifndef CONFIG_NET_MGMT_EVENT_INFO
+	ARG_UNUSED(info);
+	ARG_UNUSED(length);
+#endif /* CONFIG_NET_MGMT_EVENT_INFO */
+
+	(void)k_mutex_lock(&net_mgmt_lock, K_FOREVER);
+
+	if (cbInvocationOngoing) {
+		NET_WARN("Re-entrant call from within a callback isn't best practice");
+		//printf("[mgmt_push_event] re-entrancy detected!!!\n");
+	}
+
+	//printf("[mgmt_push_event] Placing event at index: %d, event %d, iface %p\n", head, (int)mgmt_event, iface);
+
+	events[head].event = mgmt_event;
+	events[head].iface = iface;
+
+    	advance_event_queue_pointer();
+
+
+#ifdef CONFIG_NET_MGMT_EVENT_INFO
+	if (info && length) {
+		if (length <= NET_EVENT_INFO_MAX_SIZE) {
+			memcpy(events[head].info, info, length);
+			events[head].info_length = length;
+		} else {
+			NET_ERR("Event %u info length %zu > max size %zu",
+				mgmt_event, length, NET_EVENT_INFO_MAX_SIZE);
+			(void)k_mutex_unlock(&net_mgmt_lock);
+
+			return;
+		}
+	} else {
+		events[head].info_length = 0;
+	}
+#endif /* CONFIG_NET_MGMT_EVENT_INFO */
+
+	(void)k_mutex_unlock(&net_mgmt_lock);
+}
+
+static inline struct mgmt_event_entry *mgmt_peek_event(void)
+{
+	if (!is_event_queue_full() && (head == tail)) {
+		return NULL;
+	}
+
+	return &events[tail];
+}
+
+static inline void mgmt_pop_event(void)
+{
+	retreat_event_queue_pointer();
+}
+
+static inline void mgmt_clean_event(struct mgmt_event_entry *mgmt_event)
+{
+	mgmt_event->event = 0U;
+	mgmt_event->iface = NULL;
+}
+
+static inline void mgmt_add_event_mask(uint32_t event_mask)
+{
+	global_event_mask |= event_mask;
+}
+
+static inline void mgmt_rebuild_global_event_mask(void)
+{
+	struct net_mgmt_event_callback *cb, *tmp;
+
+	global_event_mask = 0U;
+
+	SYS_SLIST_FOR_EACH_CONTAINER_SAFE(&event_callbacks, cb, tmp, node) {
+		mgmt_add_event_mask(cb->event_mask);
+	}
+}
+
+static inline bool mgmt_is_event_handled(uint32_t mgmt_event)
+{
+	return (((NET_MGMT_GET_LAYER(mgmt_event) &
+		  NET_MGMT_GET_LAYER(global_event_mask)) ==
+		 NET_MGMT_GET_LAYER(mgmt_event)) &&
+		((NET_MGMT_GET_LAYER_CODE(mgmt_event) &
+		  NET_MGMT_GET_LAYER_CODE(global_event_mask)) ==
+		 NET_MGMT_GET_LAYER_CODE(mgmt_event)) &&
+		((NET_MGMT_GET_COMMAND(mgmt_event) &
+		  NET_MGMT_GET_COMMAND(global_event_mask)) ==
+		 NET_MGMT_GET_COMMAND(mgmt_event)));
+}
+
+static inline void mgmt_run_callbacks(struct mgmt_event_entry *mgmt_event)
+{
+	sys_snode_t *prev = NULL;
+	struct net_mgmt_event_callback *cb, *tmp;
+
+	NET_DBG("Event layer %u code %u cmd %u",
+		NET_MGMT_GET_LAYER(mgmt_event->event),
+		NET_MGMT_GET_LAYER_CODE(mgmt_event->event),
+		NET_MGMT_GET_COMMAND(mgmt_event->event));
+	/*printf("Event layer %u code %u cmd %u\n",
+		NET_MGMT_GET_LAYER(mgmt_event->event),
+		NET_MGMT_GET_LAYER_CODE(mgmt_event->event),
+		NET_MGMT_GET_COMMAND(mgmt_event->event));*/
+
+	SYS_SLIST_FOR_EACH_CONTAINER_SAFE(&event_callbacks, cb, tmp, node) {
+		if (!(NET_MGMT_GET_LAYER(mgmt_event->event) ==
+		      NET_MGMT_GET_LAYER(cb->event_mask)) ||
+		    !(NET_MGMT_GET_LAYER_CODE(mgmt_event->event) ==
+		      NET_MGMT_GET_LAYER_CODE(cb->event_mask)) ||
+		    (NET_MGMT_GET_COMMAND(mgmt_event->event) &&
+		     NET_MGMT_GET_COMMAND(cb->event_mask) &&
+		     !(NET_MGMT_GET_COMMAND(mgmt_event->event) &
+		       NET_MGMT_GET_COMMAND(cb->event_mask)))) {
+			//printf("cb %p isn't interested\n", cb);
+			continue;
+		}
+
+#ifdef CONFIG_NET_MGMT_EVENT_INFO
+		if (mgmt_event->info_length) {
+			cb->info = (void *)mgmt_event->info;
+			cb->info_length = mgmt_event->info_length;
+		} else {
+			cb->info = NULL;
+			cb->info_length = 0;
+		}
+#endif /* CONFIG_NET_MGMT_EVENT_INFO */
+
+		if (NET_MGMT_EVENT_SYNCHRONOUS(cb->event_mask)) {
+			struct mgmt_event_wait *sync_data =
+				CONTAINER_OF(cb->sync_call,
+					     struct mgmt_event_wait, sync_call);
+
+			if (sync_data->iface &&
+			    sync_data->iface != mgmt_event->iface) {
+				continue;
+			}
+
+			NET_DBG("Unlocking %p synchronous call", cb);
+
+			cb->raised_event = mgmt_event->event;
+			sync_data->iface = mgmt_event->iface;
+
+			sys_slist_remove(&event_callbacks, prev, &cb->node);
+
+			k_sem_give(cb->sync_call);
+		} else {
+			NET_DBG("Running callback %p : %p",
+				cb, cb->handler);
+			/*printf("Running callback %p : %p\n",
+				cb, cb->handler);*/
+
+			cbInvocationOngoing = true;
+			cb->handler(cb, mgmt_event->event, mgmt_event->iface);
+			cbInvocationOngoing = false;
+			prev = &cb->node;
+		}
+	}
+
+#ifdef CONFIG_NET_DEBUG_MGMT_EVENT_STACK
+	log_stack_usage(&mgmt_thread_data);
+#endif
+}
+
+static void mgmt_thread(void)
+{
+	struct mgmt_event_entry *mgmt_event;
+
+	while (1) {
+		k_sem_take(&network_event, K_FOREVER);
+
+		//printf("[mgmt_thread] sem count: %d\n", k_sem_count_get(&network_event));
+		(void)k_mutex_lock(&net_mgmt_lock, K_FOREVER);
+
+		NET_DBG("Handling events, forwarding it relevantly");
+
+		mgmt_event = mgmt_peek_event();
+		if (!mgmt_event) {
+			/* System is over-loaded?
+			 * At this point we have most probably notified
+			 * more events than we could handle
+			 */
+			NET_WARN("Some event got probably lost (%u), try increasing the 'CONFIG_NET_MGMT_EVENT_QUEUE_SIZE'",
+				k_sem_count_get(&network_event));
+			//printf("ZZZZ\n");
+
+			k_sem_init(&network_event, 0, K_SEM_MAX_LIMIT);
+			(void)k_mutex_unlock(&net_mgmt_lock);
+
+			continue;
+		}
+
+		/* in the callback, a call could be done again to mgmt_push_event
+		 * causing another element to be pushed onto the queue.
+		 * Hence, we a peek is needed to keep the head and tail counter as is
+		 * until callbacks have passed.
+		 * An example of such a recursion in the mDNS mdns_iface_event_handler
+		 * calling net_ipv4_igmp_join, injecting an event
+		 */
+		mgmt_run_callbacks(mgmt_event);
+
+		mgmt_clean_event(mgmt_event);
+
+		mgmt_pop_event();
+
+		(void)k_mutex_unlock(&net_mgmt_lock);
+
+		k_yield();
+	}
+}
+
+static int mgmt_event_wait_call(struct net_if *iface,
+				uint32_t mgmt_event_mask,
+				uint32_t *raised_event,
+				struct net_if **event_iface,
+				const void **info,
+				size_t *info_length,
+				k_timeout_t timeout)
+{
+	struct mgmt_event_wait sync_data = {
+		.sync_call = Z_SEM_INITIALIZER(sync_data.sync_call, 0, 1),
+	};
+	struct net_mgmt_event_callback sync = {
+		.sync_call = &sync_data.sync_call,
+		.event_mask = mgmt_event_mask | NET_MGMT_SYNC_EVENT_BIT,
+	};
+	int ret;
+
+	if (iface) {
+		sync_data.iface = iface;
+	}
+
+	NET_DBG("Synchronous event 0x%08x wait %p", sync.event_mask, &sync);
+
+	net_mgmt_add_event_callback(&sync);
+
+	ret = k_sem_take(sync.sync_call, timeout);
+	if (ret == -EAGAIN) {
+		ret = -ETIMEDOUT;
+	} else {
+		if (!ret) {
+			if (raised_event) {
+				*raised_event = sync.raised_event;
+			}
+
+			if (event_iface) {
+				*event_iface = sync_data.iface;
+			}
+
+#ifdef CONFIG_NET_MGMT_EVENT_INFO
+			if (info) {
+				*info = sync.info;
+
+				if (info_length) {
+					*info_length = sync.info_length;
+				}
+			}
+#endif /* CONFIG_NET_MGMT_EVENT_INFO */
+		}
+	}
+
+	return ret;
+}
+
+void net_mgmt_add_event_callback(struct net_mgmt_event_callback *cb)
+{
+	NET_DBG("Adding event callback %p", cb);
+
+	(void)k_mutex_lock(&net_mgmt_lock, K_FOREVER);
+
+	sys_slist_prepend(&event_callbacks, &cb->node);
+
+	mgmt_add_event_mask(cb->event_mask);
+
+	(void)k_mutex_unlock(&net_mgmt_lock);
+}
+
+void net_mgmt_del_event_callback(struct net_mgmt_event_callback *cb)
+{
+	NET_DBG("Deleting event callback %p", cb);
+
+	(void)k_mutex_lock(&net_mgmt_lock, K_FOREVER);
+
+	sys_slist_find_and_remove(&event_callbacks, &cb->node);
+
+	mgmt_rebuild_global_event_mask();
+
+	(void)k_mutex_unlock(&net_mgmt_lock);
+}
+
+void net_mgmt_event_notify_with_info(uint32_t mgmt_event, struct net_if *iface,
+				     const void *info, size_t length)
+{
+	if (mgmt_is_event_handled(mgmt_event)) {
+		NET_DBG("Notifying Event layer %u code %u type %u",
+			NET_MGMT_GET_LAYER(mgmt_event),
+			NET_MGMT_GET_LAYER_CODE(mgmt_event),
+			NET_MGMT_GET_COMMAND(mgmt_event));
+		/*printf("Notifying Event layer %u code %u type %u\n",
+			NET_MGMT_GET_LAYER(mgmt_event),
+			NET_MGMT_GET_LAYER_CODE(mgmt_event),
+			NET_MGMT_GET_COMMAND(mgmt_event));*/
+
+		mgmt_push_event(mgmt_event, iface, info, length);
+		k_sem_give(&network_event);
+	}
+}
+
+int net_mgmt_event_wait(uint32_t mgmt_event_mask,
+			uint32_t *raised_event,
+			struct net_if **iface,
+			const void **info,
+			size_t *info_length,
+			k_timeout_t timeout)
+{
+	return mgmt_event_wait_call(NULL, mgmt_event_mask,
+				    raised_event, iface, info, info_length,
+				    timeout);
+}
+
+int net_mgmt_event_wait_on_iface(struct net_if *iface,
+				 uint32_t mgmt_event_mask,
+				 uint32_t *raised_event,
+				 const void **info,
+				 size_t *info_length,
+				 k_timeout_t timeout)
+{
+	NET_ASSERT(NET_MGMT_ON_IFACE(mgmt_event_mask));
+	NET_ASSERT(iface);
+
+	return mgmt_event_wait_call(iface, mgmt_event_mask,
+				    raised_event, NULL, info, info_length,
+				    timeout);
+}
+
+void net_mgmt_event_init(void)
+{
+	head = 0;
+	tail = 0;
+
+	(void)memset(events, 0, CONFIG_NET_MGMT_EVENT_QUEUE_SIZE *
+			sizeof(struct mgmt_event_entry));
+
+#if defined(CONFIG_NET_TC_THREAD_COOPERATIVE)
+/* Lowest priority cooperative thread */
+#define THREAD_PRIORITY K_PRIO_COOP(CONFIG_NUM_COOP_PRIORITIES - 1)
+#else
+#define THREAD_PRIORITY K_PRIO_PREEMPT(CONFIG_NUM_PREEMPT_PRIORITIES - 1)
+#endif
+
+	k_thread_create(&mgmt_thread_data, mgmt_stack,
+			K_KERNEL_STACK_SIZEOF(mgmt_stack),
+			(k_thread_entry_t)mgmt_thread, NULL, NULL, NULL,
+			THREAD_PRIORITY, 0, K_NO_WAIT);
+	k_thread_name_set(&mgmt_thread_data, "net_mgmt");
+
+	NET_DBG("Net MGMT initialized: queue of %u entries, stack size of %u",
+		CONFIG_NET_MGMT_EVENT_QUEUE_SIZE,
+		CONFIG_NET_MGMT_EVENT_STACK_SIZE);
+}
+
+#else
+
 /*
  * Copyright (c) 2016 Intel Corporation.
  *
@@ -44,6 +494,8 @@ static sys_slist_t event_callbacks = SYS_SLIST_STATIC_INIT(&event_callbacks);
 static int16_t in_event;
 static int16_t out_event;
 
+static bool cbInvocationOngoing = false;
+
 static inline void mgmt_push_event(uint32_t mgmt_event, struct net_if *iface,
 				   const void *info, size_t length)
 {
@@ -55,6 +507,10 @@ static inline void mgmt_push_event(uint32_t mgmt_event, struct net_if *iface,
 #endif /* CONFIG_NET_MGMT_EVENT_INFO */
 
 	(void)k_mutex_lock(&net_mgmt_lock, K_FOREVER);
+
+	if (cbInvocationOngoing) {
+		NET_ERR("Re-entrant call from within a callback isn't best practice");
+	}
 
 	i_idx = in_event + 1;
 	if (i_idx == CONFIG_NET_MGMT_EVENT_QUEUE_SIZE) {
@@ -210,7 +666,9 @@ static inline void mgmt_run_callbacks(struct mgmt_event_entry *mgmt_event)
 			NET_DBG("Running callback %p : %p",
 				cb, cb->handler);
 
+			cbInvocationOngoing = true;
 			cb->handler(cb, mgmt_event->event, mgmt_event->iface);
+			cbInvocationOngoing = false;
 			prev = &cb->node;
 		}
 	}
@@ -236,7 +694,7 @@ static void mgmt_thread(void)
 			 * At this point we have most probably notified
 			 * more events than we could handle
 			 */
-			NET_DBG("Some event got probably lost (%u)",
+			NET_WARN("Some event got probably lost (%u)",
 				k_sem_count_get(&network_event));
 
 			k_sem_init(&network_event, 0, K_SEM_MAX_LIMIT);
@@ -400,3 +858,5 @@ void net_mgmt_event_init(void)
 		CONFIG_NET_MGMT_EVENT_QUEUE_SIZE,
 		CONFIG_NET_MGMT_EVENT_STACK_SIZE);
 }
+
+#endif
